c766bc0b8d41735a79d166108c03302fe5cca674
Jérémy Boureux <jeremyboureux@gmail.com>
2023-05-23 02:17:45 +0200

    add symfony/apache-pack

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
 
     #[Route('/', name: 'homepage')]
-    public function index()
+    public function index() : Response
     {
         return $this->render('base.html.twig');
     }
